a5b5078ba96e4309e60928c4604b721af3c69f74
Caleb Ukle <caleb.ukle+github@pm.me>
2023-06-12 14:17:24 -0500

    c

--- apps/demo/webpack.config.js
+++ apps/demo/webpack.config.js
@@ -2,8 +2,12 @@ const { composePlugins, withNx } = require('@nx/webpack');
 const { withReact } = require('@nx/react');
 
 // Nx plugins for webpack.
-module.exports = composePlugins(withNx(), withReact(), (config) => {
+module.exports = composePlugins(withNx(), withReact(), async (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+  await new Promise((res) => {
+    setTimeout(res, 1000);
+  });
+  console.log('Webpack config:', config);
   return config;
 });
